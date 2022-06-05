1e8ece6a8a3791676d9617fad94fa6ef6e927a42
Japneet <Japneetsingh.cse19@chitkarauniversity.edu.in>
2022-06-06 01:15:02 +0530

    First Commit

---
+++ Rockpaperscissorsgamejava.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+import java.util.Random;
+public class Firstjava {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter 0 for rock, 1 for paper, 2 for scissors");
+		int u1=sc.nextInt();
+		Random random= new Random();
+		int computerinput=random.nextInt(3);
+		if(u1==computerinput)
+		{
+			System.out.println("Draw");
+		}
+		else if(u1==0 && computerinput==2 || u1 ==1 && computerinput==0 || u1==2 && computerinput==1)
+		{
+			System.out.println("You win!");
+		}
+		else
+		{
+			System.out.println("Computer win, you lose");
+		}   
+		System.out.println("Computer choose "+computerinput);
+	}
+
+}
